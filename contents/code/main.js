30f0212dd63b4fe6afcae22d8b3fc12a1731ef5a
gl00mt1t4n <181766821+gl00mt1t4n@users.noreply.github.com>
2026-08-09 23:43:49 -0400

    Pin opacity to flat 91%

--- contents/code/main.js
+++ contents/code/main.js
@@ -9,8 +9,8 @@
 const PREFIX = "wallpaper-transparency:";
 
 const config = {
-    activeOpacity: clamp(Number(readConfig("activeOpacity", 89)) / 100, 0.1, 1),
-    inactiveOpacity: clamp(Number(readConfig("inactiveOpacity", 89)) / 100, 0.1, 1),
+    activeOpacity: clamp(Number(readConfig("activeOpacity", 91)) / 100, 0.1, 1),
+    inactiveOpacity: clamp(Number(readConfig("inactiveOpacity", 91)) / 100, 0.1, 1),
     overlapThreshold: clamp(Number(readConfig("overlapThreshold", 1)) / 100, 0.01, 1),
     protectedClasses: parsePatterns(String(readConfig(
         "protectedClasses",
